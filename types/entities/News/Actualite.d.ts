8eabee4d54fcef4a5e499c55c0704b3e74a686aa
Maël Gangloff <maelgangloffd@gmail.com>
2020-12-30 01:35:34 +0100

    fix: types

--- types/entities/News/Actualite.d.ts
+++ types/entities/News/Actualite.d.ts
@@ -4,12 +4,14 @@ export default class Actualite {
     codeEmetteur: number;
     date: Date;
     uid: string;
+    titre: string;
     private errmsg;
     constructor(article: {
         errmsg: string | null;
         type: string;
         auteur: string;
         codeEmetteur: string;
+        titre: string;
         date: number;
         uid: string;
     });
